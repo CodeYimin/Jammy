d6d2c5e61da3cc138db8fee51e9a0277a02fecf8
Yimin123 <56568926+CodeYimin@users.noreply.github.com>
2022-04-23 17:31:00 +0000

    Configure replit nix

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Jammy
  A discord music bot.
-
-# Initialize on Replit:
-```sh
-npm run replit-init
-```
--- replit.nix
+++ replit.nix
@@ -1,7 +1,6 @@
 { pkgs }: {
 	deps = [
 		pkgs.nodejs-17_x
-      pkgs.nodePackages.typescript-language-server
-      pkgs.nodePackages.yarn
+    pkgs.python39Full
 	];
 }
